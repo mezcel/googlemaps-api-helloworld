368d0b61b17c2017fa4859960881954b53684029
mezcel <mezcel@mail.com>
2017-09-14 08:48:38 -0400

    clone and cleaned, added modules from a codepen example

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ this is my test atm: https://developers.google.com/maps/documentation/javascript
 Users of the standard API: Free until exceeding 25,000 map loads per 24 hours
 
 I need this for lat/lon: http://www.latlong.net/convert-address-to-lat-long.html
+
+modal inspired by: https://codepen.io/clintabrown/pen/jEOmZK?editors=0010
--- scripts/myScript.js
+++ scripts/myScript.js
@@ -1,27 +1,28 @@
 var locations = [
-    ['Auburn', '2436 East University Drive, Suite 2203-04, Auburn, AL 36830', '(334) 246-3910'],
-    ['Dothan', ' 1.1.2.1  3702 Ross Clark Circle, Suite 3 Dothan, AL 36303', '(334) 671-1315'],
-    ['Mobile','3662 Dauphin Street, Suite B, Mobile, AL 36608','(251) 476-8210']
+    ['Auburn', '2436 East University Drive, Suite 2203-04, Auburn, AL 36830', '(334) 246-3910', 32.629283, -85.463560, 1],
+    ['Dothan', '3702 Ross Clark Circle, Suite 3 Dothan, AL 36303', '(334) 671-1315'],
+    ['Mobile', '3662 Dauphin Street, Suite B, Mobile, AL 36608', '(251) 476-8210']
 ];
-
 var geocoder;
 var map;
 var bounds = new google.maps.LatLngBounds();
 
 function initialize() {
-    map = new google.maps.Map(
-    document.getElementById("map_canvas"), {
-        center: new google.maps.LatLng(37.4419, -122.1419),
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    map = new google.maps.Map(document.getElementById("map_canvas"), {
+        center: new google.maps.LatLng(32.629283, -85.463560),
+        /* regional center point initial view */
         zoom: 13,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     });
     geocoder = new google.maps.Geocoder();
-
     for (i = 0; i < locations.length; i++) {
-
-
         geocodeAddress(locations, i);
     }
+    directionsDisplay.setMap(map);
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+    ctionsDisplay.setMap(map);
 }
 google.maps.event.addDomListener(window, "load", initialize);
 
@@ -31,9 +32,7 @@ function geocodeAddress(locations, i) {
     var url = locations[i][2];
     geocoder.geocode({
         'address': locations[i][1]
-    },
-
-    function (results, status) {
+    }, function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
             var marker = new google.maps.Marker({
                 icon: 'http://maps.google.com/mapfiles/ms/icons/blue.png',
@@ -54,7 +53,7 @@ function geocodeAddress(locations, i) {
 }
 
 function infoWindow(marker, map, title, address, url) {
-    google.maps.event.addListener(marker, 'click', function () {
+    google.maps.event.addListener(marker, 'click', function() {
         var html = "<div><h3>" + title + "</h3><p>" + address + "<br></div><a href='" + url + "'>View location</a></p></div>";
         iw = new google.maps.InfoWindow({
             content: html,
@@ -78,4 +77,19 @@ function createMarker(results) {
     map.fitBounds(bounds);
     infoWindow(marker, map, title, address, url);
     return marker;
+}
+/* Detting Directions */
+//https://developers.google.com/maps/documentation/javascript/examples/directions-simple
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    directionsService.route({
+        origin: '78 River COurt, Crawfordville FL 32327',
+        destination: '3702 Ross Clark Circle, Suite 3 Dothan, AL 36303',
+        travelMode: 'DRIVING'
+    }, function(response, status) {
+        if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+        } else {
+            window.alert('Directions request failed due to ' + status);
+        }
+    });
 }
