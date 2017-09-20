67abe42953b1b91c8839e9e57ea0ac59eb422387
mezcel <mezcel@mail.com>
2017-09-20 06:54:25 -0400

    remade dir

---
+++ scrapy_virtualenv_helloworld/expresspros/expresspros/spiders/expresspros_spider.py
@@ -0,0 +1,13 @@
+import scrapy
+
+class ExpressprosSpider(scrapy.Spider):
+    name = "expresspros"
+    allowed_domain = ["https://www.expresspros.com/"]
+    start_urls = ["https://workforce.expresspros.com/locations/state/Alabama"]
+
+    # clones the webpage into an html
+
+     def parse(self, response):
+        filename = response.url.split("/")[-2] + '.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
