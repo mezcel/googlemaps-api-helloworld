c8ad9fc5bf75ab04c50aa71aaa78c76eb28840b6
mezcel <mezcel@mail.com>
2017-09-21 19:18:58 -0400

    launch url to scrape via terminal ex:scrapy crawl expresspros -a start_urls='https://workforce.expresspros.com/locations/state/Alabama' -o myjsontest.json

--- scrapy_virtualenv_helloworld/expresspros/expresspros/spiders/expresspros_spider.py
+++ scrapy_virtualenv_helloworld/expresspros/expresspros/spiders/expresspros_spider.py
@@ -3,11 +3,29 @@ from expresspros.items import ExpressprosItem
 
 class ExpressprosSpider(scrapy.Spider):
     name = "expresspros"
-    allowed_domain = ["https://www.expresspros.com/"]
-    start_urls = ["https://workforce.expresspros.com/locations/state/Alabama"]
+    # allowed_domain = ["https://www.expresspros.com/"]
+    # start_urls = ["https://workforce.expresspros.com/locations/state/Alabama"]
+
+    def __init__(self, domain='', *args,**kwargs):
+        super(ExpressprosSpider, self).__init__(*args, **kwargs)
+        self.start_urls = [domain]
+        # scrapy crawl myspider -a category='mycategory' -a domain='example.com'
+        # scrapy crawl expresspros -a domain='https://workforce.expresspros.com/locations/state/Alabama' -o myjsontest.json
+
+    # urls from terminal
+    # scrapy crawl myspider -a domain="http://www.example.com"
+    # vs
+    # scrapy crawl myspider -o filename.json
+    # https://stackoverflow.com/questions/15611605/how-to-pass-a-user-defined-argument-in-scrapy-spider
+    # https://doc.scrapy.org/en/latest/topics/spiders.html#spider-arguments
+
+
+    # scrapy crawl myspider -a parameter1=value1
+    # scrapy crawl expresspros -a start_urls='https://workforce.expresspros.com/locations/state/Alabama' -o myjsontest.json
+    # self.start_urls = start_urls
 
     def parse(self, response):
-         for sel in response.xpath('//div[@class="row location-item"][@style="padding-bottom: 15px;"]'):
+        for sel in response.xpath('//div[@class="row location-item"][@style="padding-bottom: 15px;"]'):
             item = ExpressprosItem()
 
             item['title'] = sel.xpath('h4/text()').extract()
